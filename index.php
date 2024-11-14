b831da756182abc65d6cda5ebc0497cc3e197531
Vesko2103 <veselin_g@hotmail.com>
2024-11-14 18:05:18 +0100

    creacion index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso WEb</title>
+</head>
+<body>
+    <div><h1>Curso Web</h1></div>
+    <a href="hola.php" target="_blank" rel="noopener noreferrer">Ir A Hola Mundo</a>
+    
+</body>
+</html>
