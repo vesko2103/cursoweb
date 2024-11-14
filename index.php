d8c59b1d81e666a28236ec08fc449ebd0671c437
vesko2103 <59774963+vesko2103@users.noreply.github.com>
2024-11-14 19:01:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <div><h1>Curso Web</h1></div>
     <a href="hola.php" target="_blank" rel="noopener noreferrer">Ir A Hola Mundo</a>
     <p>Esto es una redaccion en GitHub</p>
+    <h2>La nueva Rama del Arbol</h2>
     
 </body>
 </html>
